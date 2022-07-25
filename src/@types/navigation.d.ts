170cf5dfc785663bea5a991e37d716539c27b732
Julio <julionev10@gmail.com>
2022-07-25 14:55:49 -0300

    First Commit
    Projeto feito durante as aulas da rocketseat evento especial do ignitelab

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList{
+      home: underfined;
+      new: underfined;
+      details: { orderId: string};
+    }
+  }
+}
